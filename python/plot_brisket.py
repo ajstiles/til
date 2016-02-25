9866609be0d282a53eef6d237a872e87dc117b53
Adam Stiles <ajstiles@gmail.com>
2016-02-24 21:31:07 -0800

    Save brisket temperature plot to file

--- python/plot_brisket.py
+++ python/plot_brisket.py
@@ -8,4 +8,8 @@ times = [0,   20,  30,  50,  55,  60,  63,  74,  94,  114, 126, 138, 154, 168, 1
 # temps in degrees read from digital thermometer
 temps = [158, 161, 159, 161, 162, 163, 164, 169, 173, 177, 182, 186, 191, 194, 201, 203]
 plt.plot(times, temps)
-plt.show()
+
+# show the plot
+# plt.show()
+
+plt.savefig("plot_brisket.png")
